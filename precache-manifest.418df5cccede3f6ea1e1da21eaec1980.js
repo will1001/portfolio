83928f9c98cc5a4c76eff696e44bc1204221ce6b
wili1001 <wilirahmatm@gmail.com>
2020-03-18 09:36:40 +0800

    Updates

---
+++ precache-manifest.418df5cccede3f6ea1e1da21eaec1980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d75272ae48b6f44a20ec93ca359a18",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "476d9e75ee6d2132779f",
+    "url": "/portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2a760b6f574b4b455d79",
+    "url": "/portfolio/static/js/2.ef7a561d.chunk.js"
+  },
+  {
+    "revision": "6d3c4316041b5905aa31b3ff080c57e2",
+    "url": "/portfolio/static/js/2.ef7a561d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476d9e75ee6d2132779f",
+    "url": "/portfolio/static/js/main.7620fc93.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
